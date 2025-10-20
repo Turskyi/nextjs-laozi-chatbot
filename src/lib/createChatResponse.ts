e83118245d1d5b35c10d06069aeb4890ad0b1a37
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-10-19 21:33:42 -0400

    refactor: update logging and invocation method for simple chat chain

--- src/lib/createChatResponse.ts
+++ src/lib/createChatResponse.ts
@@ -175,11 +175,12 @@ export async function createChatResponse({
 
     const chain = prompt.pipe(chatModel);
 
-    console.log('Invoking simple chain...');
-    await chain.invoke({
+    console.log('Invoking simple chain stream...');
+    for await (const _ of await chain.stream({
       input: currentMessageContent,
       chat_history: chatHistory,
-    });
+    })) {
+    }
     console.log('Simple chain invoked.');
     console.log('--- Simple chat end ---');
   }
